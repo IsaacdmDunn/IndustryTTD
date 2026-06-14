830599aec5c909ffe8ea16692706fa975e0dd36b
Isaac Dunn <gandalfthebignose@gmail.com>
2026-06-14 10:49:11 +0100

    all resource icons added

--- IndustryTTD/GameManager.gd
+++ IndustryTTD/GameManager.gd
@@ -4,7 +4,7 @@ enum GameResourceTypes {Wood, Stone, Food, Pop, Ore, Charcoal, Metal}
 @export var GameResourceList: Array[int] = [500, 300, 300, 20, 0,0,0,0,0]
 @export var GameResourceLimit: Array[int] = [500, 500, 500, 100, 100,100,100,100,100]
 @export var GameResourceName: Array[String] = ["Wood", "Stone", "Food", "People", "Charcoal", "Metal", "Tools", "Niter"]
-@export var GameResourceIcon: Array[String] = ["res://Sprites/ResourceIcons/Wood.png","res://Sprites/ResourceIcons/Stone.png","res://Sprites/ResourceIcons/Food.png"]
+@export var GameResourceIcon: Array[String] = ["res://Sprites/ResourceIcons/Wood.png","res://Sprites/ResourceIcons/Stone.png","res://Sprites/ResourceIcons/Food.png","res://Sprites/ResourceIcons/Person.png","res://Sprites/ResourceIcons/Charcoal.png","res://Sprites/ResourceIcons/Metal.png","res://Sprites/ResourceIcons/Tools.png","res://Sprites/ResourceIcons/Niter.png"]
 
 @export var BuildingList: Array[BuildingOption]
 @export var ProductionMethods: Array[ProductionMethod]
--- IndustryTTD/ResourceUI.gd
+++ IndustryTTD/ResourceUI.gd
@@ -12,9 +12,19 @@ func _process(delta: float) -> void:
 	$LabelList/Wood.text = ""
 	$LabelList/Stone.text = ""
 	$LabelList/Food.text = ""
+	$LabelList/Pop.text = ""
+	$LabelList2/Iron.text = ""
+	$LabelList2/Tools.text = ""
+	$LabelList2/Coal.text = ""
+	$LabelList2/Niter.text = ""
+	
 	
 	$LabelList/Wood.append_text("[img]" + gm.GameResourceIcon[0] + "[/img]" + " " + str(gm.GameResourceList[0]) + " / " + str(gm.GameResourceLimit[0]))
 	$LabelList/Stone.append_text("[img]" + gm.GameResourceIcon[1] + "[/img]" + " " + str(gm.GameResourceList[1]) + " / " + str(gm.GameResourceLimit[1]))
 	$LabelList/Food.append_text("[img]" + gm.GameResourceIcon[2] + "[/img]" + " " + str(gm.GameResourceList[2]) + " / " + str(gm.GameResourceLimit[2]))
-	
+	$LabelList/Pop.append_text("[img]" + gm.GameResourceIcon[3] + "[/img]" + " " + str(gm.GameResourceList[3]) + " / " + str(gm.GameResourceLimit[3]))
+	$LabelList2/Iron.append_text("[img]" + gm.GameResourceIcon[5] + "[/img]" + " " + str(gm.GameResourceList[5]) + " / " + str(gm.GameResourceLimit[5]))
+	$LabelList2/Tools.append_text("[img]" + gm.GameResourceIcon[6] + "[/img]" + " " + str(gm.GameResourceList[6]) + " / " + str(gm.GameResourceLimit[6]))
+	$LabelList2/Coal.append_text("[img]" + gm.GameResourceIcon[4] + "[/img]" + " " + str(gm.GameResourceList[4]) + " / " + str(gm.GameResourceLimit[4]))
+	$LabelList2/Niter.append_text("[img]" + gm.GameResourceIcon[7] + "[/img]" + " " + str(gm.GameResourceList[7]) + " / " + str(gm.GameResourceLimit[7]))
 	pass
